63b10b383256cc913fb7df276315369bdb78458d
pmehta <pmehta@gamma.ph.liv.ac.uk>
2021-12-31 22:17:53 +0000

    added error bars to plots

--- plotting_script.py
+++ plotting_script.py
@@ -103,7 +103,7 @@ ax4.legend()
 
 #########################################################################################
 #                                GETTING DATA FROM TEXT FILE
-#                                         MATT D5 DATA (WATER)
+#                                         MATT D5/HP1 DATA for AIR/WATER
 #                                         POLAR ANGLE
 ########################################################################################
 azi_angle = []
@@ -113,74 +113,102 @@ amp_error = []
 
 nline = 91
 
-df = pd.read_csv("/user/pmehta/skli_profile_analysis/D5_water_matt.csv")
+parser = argparse.ArgumentParser(description='Specify which optic and medium')
+parser.add_argument("--list", nargs="+")
+parser.add_argument('-D5', '--D5', action='store_true')
+parser.add_argument('-HP1', '--HP1', action='store_true')
+parser.add_argument('-w', '--water', action='store_true')
+parser.add_argument('-a', '--air', action='store_true')
+args = parser.parse_args()
+
+if args.D5:
+    if args.water:
+        df = pd.read_csv("/user/pmehta/skli_profile_analysis/D5_water_matt.csv")
+    if args.air:
+        df = pd.read_csv("/user/pmehta/skli_profile_analysis/D5_air_matt.csv")
+if args.HP1:
+    if args.water:
+        df = pd.read_csv("/user/pmehta/skli_profile_analysis/HP1_water_matt.csv")
+    if args.air:
+        df = pd.read_csv("/user/pmehta/skli_profile_analysis/HP1_air_matt.csv")
+
 #print(df)
 
 #azi_0 = df[df["Azimuthal"] == 0]
 
 azi_0_angle = df.loc[df["Azimuthal"] == 0, "Polar"]
 azi_0_amplitude = df.loc[df["Azimuthal"] == 0, "Amplitude"]
-
+amp_0_error = df.loc[df["Azimuthal"] == 0, "Error"]
 
 azi_30_angle = df.loc[df["Azimuthal"] == 30, "Polar"]
 azi_30_amplitude = df.loc[df["Azimuthal"] == 30, "Amplitude"]
-
+amp_30_error = df.loc[df["Azimuthal"] == 30, "Error"]
 
 azi_60_angle = df.loc[df["Azimuthal"] == 60, "Polar"]
 azi_60_amplitude = df.loc[df["Azimuthal"] == 60, "Amplitude"]
-
+amp_60_error = df.loc[df["Azimuthal"] == 60, "Error"]
 
 azi_90_angle = df.loc[df["Azimuthal"] == 90, "Polar"]
 azi_90_amplitude = df.loc[df["Azimuthal"] == 90, "Amplitude"]
-
+amp_90_error = df.loc[df["Azimuthal"] == 90, "Error"]
 
 azi_120_angle = df.loc[df["Azimuthal"] == 120, "Polar"]
 azi_120_amplitude = df.loc[df["Azimuthal"] == 120, "Amplitude"]
-
+amp_120_error = df.loc[df["Azimuthal"] == 120, "Error"]
 
 azi_150_angle = df.loc[df["Azimuthal"] == 150, "Polar"]
 azi_150_amplitude = df.loc[df["Azimuthal"] == 150, "Amplitude"]
-
+amp_150_error = df.loc[df["Azimuthal"] == 150, "Error"]
 
 azi_180_angle = df.loc[df["Azimuthal"] == 180, "Polar"]
 azi_180_amplitude = df.loc[df["Azimuthal"] == 180, "Amplitude"]
-
+amp_180_error = df.loc[df["Azimuthal"] == 180, "Error"]
 
 azi_210_angle = df.loc[df["Azimuthal"] == 210, "Polar"]
 azi_210_amplitude = df.loc[df["Azimuthal"] == 210, "Amplitude"]
-
+amp_210_error = df.loc[df["Azimuthal"] == 210, "Error"]
 
 azi_240_angle = df.loc[df["Azimuthal"] == 240, "Polar"]
 azi_240_amplitude = df.loc[df["Azimuthal"] == 240, "Amplitude"]
-
+amp_240_error = df.loc[df["Azimuthal"] == 240, "Error"]
 
 azi_270_angle = df.loc[df["Azimuthal"] == 270, "Polar"]
 azi_270_amplitude = df.loc[df["Azimuthal"] == 270, "Amplitude"]
-
+amp_270_error = df.loc[df["Azimuthal"] == 270, "Error"]
 
 azi_300_angle = df.loc[df["Azimuthal"] == 300, "Polar"]
 azi_300_amplitude = df.loc[df["Azimuthal"] == 300, "Amplitude"]
-
+amp_300_error = df.loc[df["Azimuthal"] == 300, "Error"]
 
 azi_330_angle = df.loc[df["Azimuthal"] == 330, "Polar"]
 azi_330_amplitude = df.loc[df["Azimuthal"] == 330, "Amplitude"]
-
+amp_330_error = df.loc[df["Azimuthal"] == 330, "Error"]
 
 fig2, ax5 = plt.subplots()
-ax5.plot(azi_0_angle,azi_0_amplitude, marker='x',color = 'b',alpha = 0.5, label = 'Azimuthal = 0')
-ax5.plot(azi_0_angle,azi_30_amplitude,marker='x',color = 'g',alpha = 0.5, label = 'Azimuthal = 30')
-ax5.plot(azi_0_angle,azi_60_amplitude,marker='x',color = 'r',alpha = 0.5, label = 'Azimuthal = 60')
-ax5.plot(azi_0_angle,azi_90_amplitude,marker='x',color = 'r',alpha = 0.5, label = 'Azimuthal = 90')
-ax5.plot(azi_0_angle,azi_120_amplitude,marker='x',color = 'c',alpha = 0.5, label = 'Azimuthal = 120')
-ax5.plot(azi_0_angle,azi_150_amplitude,marker='x',color = 'm',alpha = 0.5, label = 'Azimuthal = 150')
-ax5.plot(azi_0_angle,azi_180_amplitude,marker='x',color = 'y',alpha = 0.5, label = 'Azimuthal = 180')
-ax5.plot(azi_0_angle,azi_210_amplitude,marker='x',color = 'k',alpha = 0.5, label = 'Azimuthal = 210')
-ax5.plot(azi_0_angle,azi_240_amplitude,marker='x',color = 'xkcd:sky blue',alpha = 0.5, label = 'Azimuthal = 240')
-ax5.plot(azi_0_angle,azi_270_amplitude,marker='x',color = 'xkcd:lime',alpha = 0.5, label = 'Azimuthal = 270')
-ax5.plot(azi_0_angle,azi_300_amplitude,marker='x',color = 'xkcd:orange',alpha = 0.5, label = 'Azimuthal = 300')
-ax5.plot(azi_0_angle,azi_330_amplitude,marker='x',color = 'xkcd:pink',alpha = 0.5, label = 'Azimuthal = 330')
+ax5.errorbar(azi_0_angle,azi_0_amplitude,yerr = amp_0_error, marker='x',color = 'b',alpha = 0.5, label = 'Azimuthal = 0')
+ax5.errorbar(azi_0_angle,azi_30_amplitude,yerr = amp_30_error,marker='x',color = 'g',alpha = 0.5, label = 'Azimuthal = 30')
+ax5.errorbar(azi_0_angle,azi_60_amplitude,yerr = amp_60_error,marker='x',color = 'r',alpha = 0.5, label = 'Azimuthal = 60')
+ax5.errorbar(azi_0_angle,azi_90_amplitude,yerr = amp_90_error,marker='x',color = 'r',alpha = 0.5, label = 'Azimuthal = 90')
+ax5.errorbar(azi_0_angle,azi_120_amplitude,yerr = amp_120_error,marker='x',color = 'c',alpha = 0.5, label = 'Azimuthal = 120')
+ax5.errorbar(azi_0_angle,azi_150_amplitude,yerr = amp_150_error,marker='x',color = 'm',alpha = 0.5, label = 'Azimuthal = 150')
+ax5.errorbar(azi_0_angle,azi_180_amplitude,yerr = amp_180_error,marker='x',color = 'y',alpha = 0.5, label = 'Azimuthal = 180')
+ax5.errorbar(azi_210_angle,azi_210_amplitude,yerr = amp_210_error,marker='x',color = 'k',alpha = 0.5, label = 'Azimuthal = 210')
+ax5.errorbar(azi_0_angle,azi_240_amplitude,yerr = amp_240_error,marker='x',color = 'xkcd:sky blue',alpha = 0.5, label = 'Azimuthal = 240')
+ax5.errorbar(azi_0_angle,azi_270_amplitude,yerr = amp_270_error,marker='x',color = 'xkcd:lime',alpha = 0.5, label = 'Azimuthal = 270')
+ax5.errorbar(azi_0_angle,azi_300_amplitude,yerr = amp_300_error,marker='x',color = 'xkcd:orange',alpha = 0.5, label = 'Azimuthal = 300')
+ax5.errorbar(azi_0_angle,azi_330_amplitude,yerr = amp_330_error,marker='x',color = 'xkcd:pink',alpha = 0.5, label = 'Azimuthal = 330')
 ax5.legend()
 ax5.set_xlabel("Polar angle (degrees)")
 ax5.set_ylabel("Amplitude")
 #plt.axis([30, 50, 0, 1])
+if args.D5:
+    if args.water:
+        plt.title("D5 water data")
+    if args.air:
+        plt.title("D5 air data")
+if args.HP1:
+    if args.water:
+        plt.title("HP1 water data")
+    if args.air:
+        plt.title("HP1 air data")
 plt.show()
